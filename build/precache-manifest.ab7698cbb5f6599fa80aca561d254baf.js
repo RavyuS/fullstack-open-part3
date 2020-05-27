704afc392e44ac3a45a944b3adf6d1998d001922
RavyuS <ravyusk@gmail.com>
2020-05-28 01:48:57 +0800

    Merged frontend build. Exercise 3.10 completed

---
+++ build/precache-manifest.ab7698cbb5f6599fa80aca561d254baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbcce6c7a0243f39473ed9c61a6b652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9deedfea66418447aba",
+    "url": "/static/js/2.22bbad1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22bbad1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa46cfc35c9961de478",
+    "url": "/static/js/main.5bdc58e7.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
