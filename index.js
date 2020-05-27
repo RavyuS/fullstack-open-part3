7bbb9b1be9b8b113e204505891f87db6aed0364e
RavyuS <ravyusk@gmail.com>
2020-05-28 01:29:37 +0800

    Completed Exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express')  
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json()) //Middleware
 app.use(morgan(':method :url :status - :response-time ms :req-content'))
 
